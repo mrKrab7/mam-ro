2e9ec05ad38aa20f707cf4d069b5ba62d19d781e
nkita <mr.vip.13@bk.ru>
2021-08-02 02:47:16 +0700

    2

--- src/js/slider.js
+++ src/js/slider.js
@@ -36,4 +36,24 @@ const gallery2Slider = new Swiper('.gallery-2__swiper-container', {
     // And if we need scrollbar
 
 })
+const gallery3Slider = new Swiper('.gallery-7__swiper-container', {
+    // Optional parameters
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        prevEl: '.swiper-prev',
+        nextEl: '.swiper-next',
+
+    },
+
+    // And if we need scrollbar
+
+})
+
 
